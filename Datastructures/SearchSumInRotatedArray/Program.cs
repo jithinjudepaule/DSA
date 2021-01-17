880df87a95bbdd7f8c2fba828de338bf658c5eee
jithinjudepaule <jithinjudepaule@gmail.com>
2021-01-17 09:32:32 +0530

    Updates

---
+++ Datastructures/SearchSumInRotatedArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SearchSumInRotatedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 8, 9, 10, 1, 2, 3 };
+            int sum = 13;
+
+            if (findIfSumExists(array,sum))
+            {
+                Console.WriteLine("Sum exists");
+            }
+
+            else
+            {
+                Console.WriteLine("Sum does not exist");
+            }
+        }
+
+        private static bool findIfSumExists(int[] array, int sum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
