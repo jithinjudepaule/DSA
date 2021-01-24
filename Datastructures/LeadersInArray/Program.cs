d2a335ceab8ac1fc58fed7db32e6cd6e3614c70b
jithinjudepaule <jithinjudepaule@gmail.com>
2021-01-24 12:52:56 +0530

    Updates

---
+++ Datastructures/LeadersInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeadersInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 16, 17, 4, 3, 5, 2 };
+            FindLeadersInArray(array);
+        }
+
+        private static void FindLeadersInArray(int[] array)
+        {
+            int lSum = 0;
+            int sum = 0;
+            int rSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                lSum = lSum + array[i];
+                rSum = sum- lSum;
+                if (array[i]> rSum)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
