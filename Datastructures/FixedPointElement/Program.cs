30c5ec7adfc850581ebb74164c1107314616f8f1
jithinjudepaule <jithinjudepaule@gmail.com>
2021-01-27 09:59:11 +0530

    FixedPoint and Other questions

---
+++ Datastructures/FixedPointElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FixedPointElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { -10, -5, 3, 4, 7, 9 };
+           int fixedPoint= FindFixedPoint(array,0,array.Length-1);
+            if (fixedPoint!=-1)
+            {
+                Console.WriteLine($"Fixed point is {fixedPoint}");
+            }
+        }
+
+        private static int FindFixedPoint(int[] array, int low, int high)
+        {
+            if (high > low)
+            {
+
+
+                int mid = low + high / 2;
+                if (array[mid] == mid)
+                {
+                    return mid;
+                }
+                else if (mid < array[mid])
+                {
+                  return   FindFixedPoint(array, low, mid - 1);
+                }
+
+                return FindFixedPoint(array, mid+1, high);
+            }
+            return -1;
+        }
+    }
+}
