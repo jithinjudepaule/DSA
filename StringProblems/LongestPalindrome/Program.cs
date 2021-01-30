7d6a4e6b8deb9594d1dc0be765ff1c28bbb46698
jpaul <jpaul@agero.com>
2021-01-30 13:33:31 +0530

    updates

---
+++ StringProblems/LongestPalindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LongestPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palindromeString = "aaaabbaa";
+            findLongestPalindrome(palindromeString);
+        }
+
+        private static void findLongestPalindrome(string palindromeString)
+        {
+            string s = "";
+            for (int i = palindromeString.Length-1; i >=0; i--)
+            {
+                s = s + palindromeString[i];
+            }
+            if (s==palindromeString)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
