5a78adea39cf1eb756d1aecbc2ac4a1212807223
jithinjudepaule <jithinjudepaule@gmail.com>
2021-01-31 13:09:04 +0530

    String permutation

---
+++ StringProblems/PermutationString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// basic logic --- swap-permute-swap
+class Program
+{
+	static void Main()
+    {
+        int factorial = FindFactorial(4);
+        Console.WriteLine(factorial);
+    }
+
+    private static int FindFactorial(int num)
+    {
+        if (num<=1)
+        {
+            return 1;
+        }
+        Console.WriteLine($"num={num}");
+        return num * FindFactorial(num - 1);
+    
+    }
+}
+
+
