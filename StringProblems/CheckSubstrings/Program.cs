4d0381e8ca4489848b62c6a06abe3859b4fd44d8
jpaul <jpaul@agero.com>
2021-02-04 17:51:59 +0530

    updates

---
+++ StringProblems/CheckSubstrings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckSubstrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Substrings.FindSubstrring();
+            string str = "aba";
+            int distinctCharacters = 2;
+            int substringCount = FindDistinctCount(str, distinctCharacters);
+            Console.WriteLine($"Count is {substringCount}");
+        }
+
+        private static int FindDistinctCount(string str, int distinctCharacters)
+        {
+            int count = 0;
+            HashSet<char> distinctcollection = new HashSet<char>();
+          
+            for (int i = 0; i < str.Length; i++)
+            {
+                string substr = "";
+                for (int j = i; j < str.Length; j++)
+                {
+                    if (substr.Contains(str[j]))
+                    {
+                        substr += str[j];
+                    }
+                    
+
+                    else
+                    {
+                        substr += str[j];
+                        distinctcollection.Add(str[j]);
+                        if (distinctcollection.Count > distinctCharacters)
+                        {
+                            count++;
+                            Console.WriteLine(substr);
+                            break;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ StringProblems/Pangram/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCheckPangram = "The quick brown fox jumps over the lazy dog";
+            CheckPangram(stringToCheckPangram);
+        }
+
+        private static void CheckPangram(string stringToCheckPangram)
+        {
+            bool[] alphabets = new bool[26];
+            int index = 0;
+            for (int i = 0; i < stringToCheckPangram.Length; i++)
+            {
+                if (stringToCheckPangram[i]>='A' && stringToCheckPangram[i]<='Z')
+                {
+                    index = stringToCheckPangram[i] - 'A';
+                }
+                else if (stringToCheckPangram[i] >= 'a' && stringToCheckPangram[i] <= 'z')
+                {
+                    index = stringToCheckPangram[i] - 'a';
+                }
+                alphabets[index] = true;
+            }
+            bool isPangram = true;
+            for (int i = 0; i < alphabets.Length; i++)
+            {
+
+                if (alphabets[i]==false)
+                {
+                    isPangram = false;
+                    break;
+                }
+            }
+            if (isPangram)
+            {
+                Console.WriteLine("Its Pangram");
+            }
+            else
+            {
+                Console.WriteLine("Its not a  Pangram");
+            }
+        }
+    }
+}
