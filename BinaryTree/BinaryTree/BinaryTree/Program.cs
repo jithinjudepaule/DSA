21a1116be5c5c06bcc48929d2a3278cc38d4d969
jithinjudepaule <jithinjudepaule@gmail.com>
2021-04-14 08:49:29 +0530

    Binary Tree probs

---
+++ BinaryTree/BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinaryTree
+{
+
+    public class Node
+    {
+        int item;
+        public Node left, right;
+
+        public Node(int key)
+        {
+            item = key;
+            left = right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node root;
+        public BinaryTree(int key)
+        {
+            root = new Node(key);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/BinaryTreeInsertion/BinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryTree
+{
+    public static Node root;
+    static void Main()
+    {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        Console.Write("Inorder traversal before insertion:");
+        traversTree(root);
+        insert(root, 12);
+        Console.WriteLine("Inorder traversal after insertion:");
+        traversTree(root);
+    }
+
+    private static void traversTree(Node tempNode)
+    {
+        if (tempNode==null)
+        {
+            return;
+        }
+
+        traversTree(tempNode.left);
+        Console.Write($"{tempNode.key} ");
+        traversTree(tempNode.right);
+    }
+
+    private static void insert(Node temp, int key)
+    {
+        Queue<Node> nodeQueue = new Queue<Node>();
+        nodeQueue.Enqueue(temp);
+       
+
+        while (nodeQueue!=null)
+        {
+            temp = nodeQueue.Peek();
+            nodeQueue.Dequeue();
+            if (temp.left == null)
+            {
+                temp.left = new Node(12);
+                break;
+            }
+            else
+            {
+                nodeQueue.Enqueue(temp.left);
+            }
+
+            if (temp.right == null)
+            {
+                temp.right = new Node(12);
+                break;
+            }
+            else
+            {
+                nodeQueue.Enqueue(temp.right);
+            }
+
+        }
+       
+    }
+
+    public class Node
+    {
+      public  int key;
+       public  Node left, right;
+        
+        public Node(int key)
+        {
+            this.key = key;
+          
+        }
+
+      
+    }
+}
