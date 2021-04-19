ad1d2435543053cc649931e53b1aa23fe5cf837f
jpaul <jpaul@agero.com>
2021-04-19 08:54:14 +0530

    updates

---
+++ Angular-WebApi/myApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
